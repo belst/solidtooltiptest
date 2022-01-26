1c241c33acea305c510642f2c28a3f1a42176129
belst <git@bel.st>
2022-01-26 21:26:13 +0100

    testing environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
-  },
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+  console.log(process.env);
+  return {
+    plugins: [solidPlugin()],
+    base: process.env.BASEPATH || '/',
+    build: {
+      target: 'esnext',
+      polyfillDynamicImport: false,
+    },
+  }
 });
